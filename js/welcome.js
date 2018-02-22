681f5fef0e6d90e66be523e628131f984b9a5454
Taylor Bailey <taylor.williams.bailey@gmail.com>
2018-02-22 13:19:09 -0600

    welcome js and home page scss

---
+++ js/welcome.js
@@ -0,0 +1,10 @@
+"use strict";
+console.log("welcome.js working");
+
+let printDiv = document.getElementById("print");
+
+function welcome() {
+    printDiv.innerHTML=`<h2 id="parkName">VIDEO GAME LAND</h2><h1 id="welcomeCall">TAP TO VIEW THE MAP!</h1><br><img id="logo" src=../images/logo.png>`;
+}
+
+module.exports = {welcome};
