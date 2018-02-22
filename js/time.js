5474d935d1ae1994d53578140675c40e73029f90
Lindsay Mulhollen <lindsaymulhollen@Lindsays-MacBook-Pro.local>
2018-02-22 13:16:29 -0600

    dropdown

--- js/time.js
+++ js/time.js
@@ -5,11 +5,11 @@ function currentTime(){
     let c = d.toLocaleTimeString();
     console.log(c);
     let h = d.getHours();
-    console.log("hours",h);
+    // console.log("hours",h);
 }
 
 function attractionData(id){
-    console.log('get area call');
+  
   return new Promise((resolve,reject) => {
     var loader = new XMLHttpRequest();
     
@@ -31,13 +31,13 @@ attractionData()
     // The first callback function will be invoked when you resolve
     function(json_data) {
       json_data.forEach((item)=>{
-        console.log(item.times);
-        // let aTimes = [];
-        // for (let i=0;i<aTimes.length;i++){
-        //     console.log("bitch please");
-        // }
+        console.log("times", item.times);
+        let aTimes = [];
+        for (let i=0;i<aTimes.length;i++){
+            console.log("bitch please");
+        }
       });
-       console.log("API call successful and responded with", json_data);
+      //  console.log("API call successful and responded with", json_data);
     },
     // The second callback function will be invoked when you reject
     function(json_data) {
