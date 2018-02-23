a101b42160abc8d2abe2b43fb9bc10297ebf659d
Lindsay Mulhollen <lindsaymulhollen@Lindsays-MacBook-Pro.local>
2018-02-23 11:49:58 -0600

    search functionality

--- js/search.js
+++ js/search.js
@@ -3,20 +3,24 @@
 let searchDisplay = [];
 let searchNames;
 let searchLoc;
-let searchTerm = "Meet";
-// document.getElementById("searchField").value;
+let searchTerm;
 let searchFooter = document.getElementById("footer-nav");
 let printDiv = document.getElementById("print");
+let searchButton = document.getElementById("search-btn");
 
 let inventory = [];
 
 const searchHead = `
-    <div class=".headerbkgsearch">
-    <input id="searchField" type="text" value="enter query here..."></input>
-    <br>
-    <button class="btn" id="search-btn">search</button>
-    </div>`;
+<div class=".headerbkgsearch">
+<input id="searchField" type="text" prototype="enter here"></input>
+<br>
+<button class="btn" id="search-btn">search</button>
+</div>`;
 
+function grabData(){
+    searchTerm = document.getElementById("searchField").value;
+
+}
 
 function loadSearch() {
     console.log('search call');
@@ -35,44 +39,40 @@ function loadSearch() {
     });
 }
 
-loadSearch().then( 
-    (json) => {
-    
-        for (var i = 0; i < json.attractions.length; i++){
-
-            let currentSearch = json.attractions[i].name ;   
-            if (currentSearch.includes(searchTerm)){
-                searchDisplay.push(currentSearch);
-                // console.log("search display", searchDisplay);
-                printSearchData();
+function onEnter(){
+    loadSearch().then( 
+        (json) => {
+        
+            for (var i = 0; i < json.attractions.length; i++){
+                console.log("search term", searchTerm);
+                grabData();
+                let currentSearch = json.attractions[i].name ;   
+                if (currentSearch.includes(searchTerm)){
+                    searchDisplay.push(currentSearch);
+                    // console.log("search display", searchDisplay);
+                }
             }
+        },
+        (reject) => {
+            console.log("SOMETHING WENT REALLY WRONG");
         }
-    },
-    (reject) => {
-        console.log("SOMETHING WENT REALLY WRONG");
-    }
-    // function searchData(data){
-    //     let keys = Object.keys(data);
-    //     keys.forEach((item) => {
-    //         data[item].firebaseID = item;
-    //         inventory.push(data[item]);
-    //         console.log("inventory", inventory);
-    //         search(searchTerm);
-    //     });
-    // }
-);
 
-
-function printSearchData() {
-    printDiv.innerHTML = `${searchHead}`;
+    );
+}
+function populate() {
     for (let q = 0; q < searchDisplay.length; q++) {
         let currentName = searchDisplay[q];
         // let currentLocation = locationArray[q];
-        printDiv.innerHTML += `${currentName}<br>`;
-
-
-
-    }
+        printDiv.innerHTML += `${currentName}<br>`;  
+    } 
 }
 
-// document.body.addEventListener('click', timeFunction, true);
+function printSearchData() {
+    printDiv.innerHTML = `${searchHead}`;
+
+}
+
+printSearchData();
+
+
+// searchButton.addEventListener("click", grabData());
